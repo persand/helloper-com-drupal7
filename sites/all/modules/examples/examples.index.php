7775bc40f8a5c65e0b33f50e377568381e3ccfd2
Per Sandström <per@helloper.com>
2012-06-29 18:47:19 +0200

    Updated Examples module

--- sites/all/modules/examples/examples.index.php
+++ sites/all/modules/examples/examples.index.php
@@ -8,8 +8,12 @@
  * - @link ajax_example.module Using AJAX forms @endlink
  * - @link batch_example.module Using the batch API @endlink
  * - @link block_example.module Defining blocks @endlink
+ * - @link cache_example.module Demonstrate Drupal caching @endlink
+ * - @link contextual_links_example.module Defining contextual links @endlink
+ * - @link cron_example.module Using hook_cron() and hook_cron_queue_info() @endlink
  * - @link dbtng_example.module Database examples (DBTNG) @endlink
  * - @link email_example.module Sending e-mail @endlink
+ * - @link entity_example.module Creating and managing a custom entity @endlink
  * - @link field_example.module Defining fields in the field API @endlink
  * - @link file_example.module Demonstrates file handling @endlink
  * - @link filter_example.module Defining an input filter @endlink
@@ -21,9 +25,14 @@
  * - @link node_access_example.module Define custom node access fules using node access hooks @endlink
  * - @link node_example.module Creating custom node types, with fields @endlink
  * - @link page_example.module Creating a custom page @endlink
+ * - @link pager_example.module Using a table with a pager @endlink
  * - @link queue_example.module Using the Queue API @endlink
+ * - @link rdf_example.module Implementing RDF in Drupal @endlink
  * - @link render_example.module Demonstrates the render API @endlink
  * - @link simpletest_example.module Writing tests for Drupal @endlink
+ * - @link tablesort_example.module Use a render array to present a table with automatic sorting @endlink
+ * - @link tabledrag_example.module Demonstrates tabledrag forms @endlink
+ * - @link theming_example.module Demonstrate how modules shoule use theme functions and provide tpl.php files @endlink
  * - @link token_example.module Using tokens @endlink
  * - @link trigger_example.module Implementing triggers and actions @endlink
  * - @link vertical_tabs_example.module Using vertical tabs @endlink
